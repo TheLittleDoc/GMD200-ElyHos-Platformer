93d56ee313d8c5cf92252613046415590f1a5809
Elysium Hosack <elysium.hosack@mymail.champlain.edu>
2024-02-19 02:45:42 -0500

    Temporary collectable counter

---
+++ Platformer/Assets/Scripts/BoltCollect.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoltCollect : MonoBehaviour
+
+
+{
+
+    public int boltCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bolt"))
+        {
+            boltCount++;
+            Destroy(collision.gameObject);
+        }
+    }
+}
